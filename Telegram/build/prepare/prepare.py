e000b64bd71972e47a3582d8b4a9996dc136e149
ǝɔ∀ǝdʎz∀ɹɔ 👽 <665889+crazypeace@users.noreply.github.com>
2026-07-15 16:53:26 +0530

    ci: fix ALL -debug-and-release to -release (win+mac path)

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -1609,7 +1609,7 @@ mac:
 
     CONFIGURATIONS=-debug
 release:
-    CONFIGURATIONS=-debug-and-release
+    CONFIGURATIONS=-release
 mac:
     ./configure -prefix "$USED_PREFIX/Qt-$QT" \
         $CONFIGURATIONS \
@@ -1649,7 +1649,7 @@ win:
 
     SET CONFIGURATIONS=-debug
 release:
-    SET CONFIGURATIONS=-debug-and-release
+    SET CONFIGURATIONS=-release
 win:
     """ + removeDir('"%LIBS_DIR%\\Qt' + qt + '"') + """
     SET MOZJPEG_DIR=%LIBS_DIR%\\mozjpeg
